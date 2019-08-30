698fb9b0ade2616b5c33cf0e12edd7b108aef175
Marco <marcooperdomo@gmail.com>
2019-08-30 16:07:23 -0500

    Controladores en Laravel

---
+++ app/Http/Controllers/CategoriaController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+// Importado
+use App\Categoria;
+
+class CategoriaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        $categorias = Categoria::all();
+
+        return $categorias;
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        $categoria = new Categoria();
+
+        $categoria->nombre = $request->nombre;
+        $categoria->description = $request->descripcion;
+        $categoria->condicion = "1";
+
+        $categoria->save();
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
+    {
+        
+        $categoria = Categoria::findOrfail($request->id);
+
+        $categoria->nombre = $request->nombre;
+        $categoria->description = $request->descripcion;
+        $categoria->condicion = "1";
+
+        $categoria->save();
+
+    }
+
+    /**
+     * Función para determinar si la categoría está inactiva - Cambia el campo condición de la tambla categorias
+    */
+    public function desactivar(Request $request)
+    {
+
+        $categoria = Categoria::findOrfail($request->id);
+
+        $categoria->condicion = "0";
+
+        $categoria->save();
+
+    }
+
+    /**
+     * Función para determinar si la categoría está activa - Cambia el campo condición de la tambla categorias
+    */
+    public function activar(Request $request)
+    {
+
+        $categoria = Categoria::findOrfail($request->id);
+
+        $categoria->condicion = "1";
+
+        $categoria->save();
+
+    }
+
+
+}
